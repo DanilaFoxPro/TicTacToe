1e32c20d3be13dc837f6848c7f2493de244488bd
yemine7 <77396320+yemine7@users.noreply.github.com>
2021-01-14 00:30:59 +0530

    Made the program work for two passwords

--- main.c
+++ main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-
 int main()
 {
 	char password[20] = "human";
-	
+	char password2[20]= "daniel";
 	printf( "Hello world!\n" );
 	printf( "Enter your password:" );
-	
 	char user_input[20];
 	gets(user_input);
 	
-	if( strcmp( user_input, password ) == 0 ) {
-		printf("The password is correct!");
-	} else {
-		printf("Wrong password!");
+	if( strcmp( user_input, password ) == 0) { 
+		printf("The password is correct!"); //input is compared with human
+	}
+
+	
+ 	else if(strcmp(user_input,password2)== 0){
+		printf("The password is correct"); //input is compared with daniel
+	}
+	
+	else{
+		printf("Wrong password"); //the password entered is not human or daniel
 	}
 	
 	return 0;
-	
 }
