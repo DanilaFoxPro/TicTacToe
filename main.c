e70f9b3ec7a93d50ce04c9e80a5e6f82a95d7efe
Fox <danila0420@gmail.com>
2021-01-13 18:47:48 +0100

    'Better' formatting for main.c

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
+
 int main()
 {
 	char password[20] = "human";
-	char pass[20];
+	
 	printf( "Hello world!\n" );
-	printf("enter your password: ");
-	gets(pass);
-	if(strcmp ( password, pass)==0)
-	{
-		printf("the password is correct");
+	printf( "Enter your password:" );
+	
+	char user_input[20];
+	gets(user_input);
+	
+	if( strcmp( user_input, password ) == 0 ) {
+		printf("The password is correct!");
+	} else {
+		printf("Wrong password!");
 	}
-	else
-	{
-		printf("wrong password");
-	}
+	
 	return 0;
+	
 }
