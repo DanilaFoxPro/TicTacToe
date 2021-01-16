347024ad520589521701e9c6104a86227e6c475c
yemine7 <77396320+yemine7@users.noreply.github.com>
2021-01-17 00:16:40 +0530

    Updating SSS game
    The game is not completed yet, some bugs need to be fixed.

--- main.c
+++ main.c
@@ -1,30 +1,65 @@
 #include<stdio.h>
-#include<string.h>
-int scompare(char* string1, char* string2)
+#include <string.h>
+int operation( char* user_input, char* swing, char* stab,char* shield )
 {
-	if(strcmp(string1,string2)==0){ //if string1 and string2 are equal then it returns 1 else 0
-		
-		return 1;
-	}
-	else{
-		
-		return 0;
-	}
+     if(strcmp(user_input,swing)==0){ //if input is swing, the function returns 1
+     	return 1;
+	 }
+	 else if (strcmp(user_input,stab)==0){ //if input is stab, the function returns 2
+	 	return 2;
+	 }
+	 else if (strcmp(user_input,shield)==0){ //if input is shield, the function returns 3
+	 	return 3;
+	 }
+	 else{ //if input is any random alphabet or number, the function returns 0
+	 	return 0;
+	 }
 }
+
 int main()
 {
-	char string1[] = "entry";
-	char string2[] = "random";
-     //scompare(string1,string2);
-     printf("%d\n",     scompare(string1,string2)); //printing return value of scompare function
-	if(scompare(string1,string2)  ) //comparing string1 and string2 using the above function
-	{
-		printf("strings are equal");
-	}
-	else
 	
+	int health = 100;
+	char swing[]= "swing";
+	char stab[] = "stab";
+	char shield[] = "shield";
+	printf( "Hello world!\n" );
+	printf( " WELCOME TO SSS GAME" );
+	printf("The barrels health is initially 100, you win when the barrels health falls to zero. \n");
+	printf("Keep attacking!!\n");
+		printf("You can perform the following actions \n 1. Swing \t 2. Stab \t 3. Shield \n");
+
+	
+	char user_input[8];
+	gets(user_input); 
+	int i;
+	//operation(user_input,swing,stab,shield);
+	//printf("return value of function is %d\n", operation( user_input, swing,  stab, shield ));
+	while(i<2)
 	{
-		printf("strings are not equal");
+		
+    if(operation(user_input, swing ,stab,shield)){ //user input is compared with swing 
+	printf("health falls by 5 percent \n");
+	health = health - (health*0.05); //need to fix this
+	printf("health of barrel is %d \n",health); 
+		}
+		
+	
+ 	else if(operation(user_input,swing,stab,shield)){ //input is compared with stab
+ 		printf("health falls by 20 percent");
+ 		health = health - (health*0.2); //need to fix this
+ 		printf("health of the barrel is %d \n",health);
 	}
 	
+	else if (operation(user_input,swing,stab,shield)){ //input is compared with shield
+		printf("You are protected, health remains same \n Health of the barrel is %d \n", health);
+	
+	}
+	else{
+		printf("Enter valid attack option \n"); //to print when proper option is not entered
+	}
+	i++;
 }
+}
+	
+
